225f7a880f9afb3beb7c795a3c3a95ba69ac0605
AngeloCrisci <angelocrisci1998@gmail.com>
2024-10-07 12:22:56 +0200

    Added css

--- index.php
+++ index.php
@@ -7,12 +7,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP HOTEL</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+
     </head>
 
 <body>
-
-
+    <h1 class="text-center">
+        SCRIVI UNA PAROLA PALINDROMA
+    </h1>
+    <form action="">
+        <div class="container">
+            <div>
+                <label for="word"></label>
+                <input class="form-control form-control-lg mb-3" type="text" id="word" name="word">
+            </div>
+        
+            <button type="submit" class="btn btn-primary" class="">Controlla!!</button>
+        </div>
+    </form>
 
 </body>
 </html>
